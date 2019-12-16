f9f7875976b510d5f86fa89d17dac60c118cd9b3
Oliver Lockwood <olockwood@dalet.com>
2019-12-16 09:39:56 +0000

    Don't republish artifacts (#2)

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 echo "Deriving version using 'git describe'..."
 git describe
 export VERSION=$(git describe | sed 's/vfs-gcs-//g')
-echo "Will publish version: ${VERSION}"
+echo "Publishing version: ${VERSION}"
 
-mvn deploy -DperformRelease=true -Drevision=${VERSION}
+status=$(curl -s --head -w %{http_code} -o /dev/null https://dl.bintray.com/dalet-oss/maven/com/dalet/celarli/commons/vfs-gcs/${VERSION}/)
+if [ $status -eq 200 ]
+then
+  echo "Version already published - nothing to do here"
+else
+  mvn deploy -DperformRelease=true -Drevision=${VERSION}
+fi
